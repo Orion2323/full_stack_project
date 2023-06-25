d10a6ada3c80914492f1dbae7d6890b8918016c0
Giancarlos Dominguez <Giancarlosd@smu.edu>
2023-06-25 12:37:45 -0500

    Create SQL_Schema.sql

---
+++ backend/SQL_Schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE user (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255),
+    password VARCHAR(255)
+);
+
+INSERT INTO user
+VALUES
+(1, 'gian2323@gmail.com', 'password');
