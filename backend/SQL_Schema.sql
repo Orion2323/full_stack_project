83f205e50bc0dd1d779ba24ce1368c75a3f9fac9
Giancarlos Dominguez <Giancarlosd@smu.edu>
2023-06-27 12:54:24 -0500

    Implemented middleware

--- backend/SQL_Schema.sql
+++ backend/SQL_Schema.sql
@@ -1,9 +1,10 @@
-CREATE TABLE user (
+CREATE TABLE IF NOT EXISTS user (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR (255),
+    last_name VARCHAR (255),
     email VARCHAR(255),
-    password VARCHAR(255)
+    password VARCHAR(255),
+    salt VARCHAR(255)
 );
 
-INSERT INTO user
-VALUES
-(1, 'gian2323@gmail.com', 'password');
+TRUNCATE TABLE user;
