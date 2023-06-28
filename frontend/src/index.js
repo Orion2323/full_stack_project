7fe8d326b1f4266ada6ed3387d926c4bac529e14
Giancarlos Dominguez <Giancarlosd@smu.edu>
2023-06-27 23:14:41 -0500

    Implemented functional landing page skeleton

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,10 +5,13 @@ import App from './app';
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
