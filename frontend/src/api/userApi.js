46375897dd7f9b6526ed09cb598e577d0bd08d59
Giancarlos Dominguez <Giancarlosd@smu.edu>
2023-06-29 22:19:55 -0500

    Update userApi.js

--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -2,9 +2,16 @@ import axios from "axios";
 
 const URL="http://localhost:8000";
 
+export const getUserbyUsername = (username) => {
+    console.log("getting" + username)
+    return axios.get(URL + "/login/" +  username)
+    .then(x=>x.data)
+    .catch(err=>err)
+}
+
 export const createUser = (user) => {
     console.log("creating" + user)
-    return axios.post(URL + "/signUp", user)
+    return axios.post(URL + "/register", user)
     .then(x=>x.data)
     .catch(err=>err)
 }
