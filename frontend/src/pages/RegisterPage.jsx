3d9a1e0d018b278289df3cfa659398f7b4e0b476
Giancarlos Dominguez <Giancarlosd@smu.edu>
2023-06-29 22:20:18 -0500

    Implemented register page

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,7 +1,71 @@
 import React, { useState, useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { TextField } from '../components';
 
 export const RegisterPage = () => {
+    const [ firstName, setFirstName ] = useState('');
+    const [ lastName, setLastName ] = useState('');
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword] = useState('');
+
     return <> 
-        <div> Loading Register Page... </div>
+        <div className="container-fluid">  
+            <div className="navbar navbar-expand-lg py-2 p-2 bg-success mx-5" aria-label="breadcrumb">
+                <ol className="breadcrumb">
+                    <li className="breadcrumb-item active"><a href="/" className="text-decoration-none text-white bg-light-xl"> MyLibraryWebApp</a></li>
+                    <li className="breadcrumb-item active text-white" aria-current="page"> Register </li>
+                </ol>
+            </div>
+
+            <h1 className="display-4 text-center"> Register </h1>
+
+            <div className="mx-5">
+
+                <TextField 
+                    id="firstName_field"
+                    label="First Name"
+                    placeHolder="Enter first name" 
+                    input_className="form-control "                
+                    value={firstName} 
+                    setValue={setFirstName}
+                />
+
+                <TextField 
+                    id="lastName_field"
+                    label="Last Name"
+                    placeHolder="Enter last name" 
+                    input_className="form-control"                
+                    value={lastName} 
+                    setValue={setLastName}
+                />
+
+                <TextField 
+                    id="email_field"
+                    label="Email"
+                    placeHolder="Enter email" 
+                    input_className="form-control"                
+                    value={email} 
+                    setValue={setEmail}
+                />
+
+                <TextField 
+                    id="password_field"
+                    label="Password"
+                    placeHolder="Password"
+                    input_className="form-control"  
+                    value={password} 
+                    setValue={setPassword}
+                />
+                <div className="text-center d-grid gap-4 mx-auto">
+                    <Link to="/"> 
+                        <button type="button" className="btn btn-outline-primary btn-lg col-2"> Sign up </button>
+                    </Link>
+
+                    <Link to="/">
+                        <button type="button" className="btn btn-outline-danger btn-lg col-2"> Back </button>
+                    </Link>
+                </div>
+            </div>
+        </div>
     </>;
 }
