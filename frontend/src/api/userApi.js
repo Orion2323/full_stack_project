fc12e2e66aaf0ceb1283a613e6a0f5f36cf46097
Giancarlos Dominguez <Giancarlosd@smu.edu>
2024-03-07 01:52:08 -0600

    Updated URL path

--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -1,17 +1,24 @@
 import axios from "axios";
 
-const URL="http://localhost:8000";
+const SessionURL="http://localhost:9000/session";
 
 export const getUserbyUsername = (username) => {
     console.log("getting" + username)
-    return axios.get(URL + "/login/" +  username)
+    return axios.get(SessionURL + "/login/" +  username)
     .then(x=>x.data)
     .catch(err=>err)
 }
 
 export const createUser = (user) => {
     console.log("creating" + user)
-    return axios.post(URL + "/register", user)
+    return axios.post(SessionURL + "/register", user)
     .then(x=>x.data)
     .catch(err=>err)
 }
+
+const APIList = {
+    getUserbyUsername,
+    createUser
+}
+
+export default APIList
