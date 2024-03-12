b9063ea71a3648b198daf2f63d79aeea7e15a14a
Giancarlos Dominguez <Giancarlosd@smu.edu>
2024-03-11 21:32:06 -0500

    Update InnerLandingPage.jsx

--- frontend/src/pages/InnerLandingPage.jsx
+++ frontend/src/pages/InnerLandingPage.jsx
@@ -1,10 +1,31 @@
 import React, { useState, useEffect, useContext, FormEvent} from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { Box, Button, Container, FormHelperText, TextField, Typography, } from '@mui/material';
-import { LoadingButton } from '@mui/lab';
+import { useNavigate } from 'react-router-dom';
+import { AppBar, Box, Button, Container, FormHelperText, TextField, Typography, Breadcrumbs, Link, IconButton, Toolbar} from '@mui/material';
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
 
 export const InnerLandingPage = () => {
+
+    const navigate = useNavigate();
+
     return <>
-        <Typography component='h1' variant='h5'> Welcome! </Typography>
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static" color='success'>
+                <Toolbar>
+                    <IconButton size="large" edge="start" color="black" aria-label="menu" sx={{ mr: 2 }}>
+                        <MenuRoundedIcon />
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}> MyLibraryWebApp </Typography>
+                    <IconButton>
+                        <AccountCircleRoundedIcon size='large'/>
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+            <Breadcrumbs backgroundColor='#b2ffdd' className='p-2'>
+                <Box ml={9}>
+                    <Link underline='hover' href='/' color='black'> Home </Link>
+                </Box>
+            </Breadcrumbs>
+        </Box>
     </>
 }
